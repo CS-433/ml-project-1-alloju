58f4d2f4488d895f06af0f4d96d053ef4c01b9b1
Louise Anne Marie Font <louise.font@gmail.ch>
2022-10-20 21:39:54 +0200

    replace outliers

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 data_path = "data/train.csv"
 x,y = load_data(data_path)
 
+def get_id(data_path):
+    id = np.genfromtxt(data_path, delimiter=",", skip_footer=250000, dtype = str)
+    return id[2:-1]
+
 def missing_data(x): # à voir si on utilise mean ou median
     d = x.shape[1] # determine the number of features
     for i in range(d):
@@ -33,11 +37,13 @@ def angle_values(x):
         colomns = colomns + 1
     return x
 
-def plot_hist(x0, x1):
-    for i in range (len(x0[0])):
+def plot_hist(x0, x1, id):
+    #for i in range (len(x0[0])):
+    for i in range (4):
         plt.hist(x0.T[i], bins = 100, density = True, label = 'Background', alpha = 0.5)
         plt.hist(x1.T[i], bins = 100, density = True, label = 'Signal', alpha = 0.5)
         plt.legend()
+        plt.title(id[i])
         plt.show()
     return None
 
@@ -47,15 +53,36 @@ def boxplot(x):
         plt.show()
     return None
 
-def preproc(x):
-    x = missing_data(x)
-    x = angle_values(x)
-    x, x_mean, x_std = standardize(x)
+def remove_outliers(x):
+    for i in range(len(x[1])):
+        feature_removed = x[:,i][x[:,i] != -999]
+        qs = np.quantile(feature_removed, np.array([0.25, 0.5, 0.75]), axis = 0)
+        ir = qs[2]-qs[0]
+        lower_limit = qs[0] - (1.5*ir)
+        upper_limit = qs[2] + (1.5*ir)
+        x[:, i][x[:, i] < lower_limit] = qs[1]
+        x[:, i][x[:, i] > upper_limit] = qs[1]
+        x[:, i][x[:, i] == -999] = qs[1]
     return x
 
-x =missing_data(x)
-x = angle_values(x) 
-x, x_mean, x_std = standardize(x) 
+def preproc(x):
+
+    x = remove_outliers(x)
+    x = angle_values(x)
+    #x, x_mean, x_std = standardize(x)
+    return x
+
+def replace_class(x):
+    #TODO replace argument 0,1,2,3 by 4 different colomns containing 0 and 1
+    return x
+
+def corr(x):
+    #TODO check which attributes have high correlation, if above a certain threshold, delete one of them
+    return x
+
+x= preproc(x)
+id = get_id(data_path)
 x0,x1 = separate(x, y)
-#plot_hist(x0,x1)
-boxplot(x)
+plot_hist(x0,x1,id)
+#boxplot(x)
+
