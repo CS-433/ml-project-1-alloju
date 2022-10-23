f2bb29093533a700b2d45b502b8edb4215cd030f
Louise Anne Marie Font <louise.font@gmail.ch>
2022-10-23 14:20:30 +0200

    preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -11,12 +11,11 @@ def get_id(data_path):
     id = np.genfromtxt(data_path, delimiter=",", skip_footer=250000, dtype = str)
     return id[2:-1]
 
-def missing_data(x): # à voir si on utilise mean ou median
-    d = x.shape[1] # determine the number of features
-    for i in range(d):
-        feature_removed = x[:,i][x[:,i] != -999] #remove all features equal to -999 (undetermined)
-        mean = np.mean(feature_removed) #determine the mean
-        x[:,i][np.isclose(x[:,i],-999)]=mean #replace undetermined values by the mean
+def missing_data(x): 
+    # define colomns with too much missing data
+    special = [4,5,6,12,23,24,25,26,27,28]
+    for i in special :
+        x[:,i][x[:,i] == -999] = 0
     return x
 
 def separate(x,y):
@@ -26,10 +25,11 @@ def separate(x,y):
 
 def angle_values(x):
     colomns = np.array([15,18,20,25,28]) #index of colomns containing the colomns with angle values
-    for i in range(len(colomns)):
+    for j in range(len(colomns)):
+        i = colomns[j]
         sin = np.sin(x[:,i]) # compute sin of angle
         cos = np.cos(x[:,i]) # compute cos of angle
-        x = np.delete(x, colomns[i], axis = 1) # delete angle value
+        x = np.delete(x, i, axis = 1) # delete angle value
         x = np.insert(x, i, sin, axis = 1) # insert sin
         x = np.insert(x, i, cos, axis = 1) # insert cos
         colomns = colomns + 1
@@ -65,22 +65,21 @@ def remove_outliers(x):
 def replace_class(x):
     len_ = np.shape(x)[0]
     #create 4 new colomns with indexes 25 to 28 containing solely zeros
-    x = np.insert(x, 30, np.zeros(len_), axis = 1)
-    x = np.insert(x, 30, np.zeros(len_), axis = 1)
-    x = np.insert(x, 30, np.zeros(len_), axis = 1)
-    x = np.insert(x, 30, np.zeros(len_), axis = 1)
+    x = np.insert(x, 26, np.zeros(len_), axis = 1)
+    x = np.insert(x, 26, np.zeros(len_), axis = 1)
+    x = np.insert(x, 26, np.zeros(len_), axis = 1)
+    x = np.insert(x, 26, np.zeros(len_), axis = 1)
     #replace indixes that are missing
-    x[:, 30][x[:,29] == 0] = 1
-    x[:, 31][x[:,29] == 1] = 1
-    x[:, 32][x[:,29] == 2] = 1
-    x[:, 33][x[:,29] == 3] = 1
+    x[:, 26][x[:,25] == 0] = 1
+    x[:, 27][x[:,25] == 1] = 1
+    x[:, 28][x[:,25] == 2] = 1
+    x[:, 29][x[:,25] == 3] = 1
     #delete the colomn
-    x = np.delete(x, 29, axis = 1)
+    x = np.delete(x, 25, axis = 1)
     return x
 
 def corr(x):
     #TODO check which attributes have high correlation, if above a certain threshold, delete one of them
-    #corr = np.zeros([x.shape[1], x.shape[1]])
     corr = np.corrcoef(x,rowvar = False)
     corr[:][np.isclose(corr,1)] = 0
     corr[:][corr < 0.95] = 0
@@ -90,11 +89,12 @@ def corr(x):
     return x
 
 def preproc(x):
+    x = missing_data(x)
     x = remove_outliers(x)
     x = angle_values(x)
     x = replace_class(x)
     #x = corr(x)
-    x, x_mean, x_std = standardize(x)
+    #x, x_mean, x_std = standardize(x)
     return x
 
 x= preproc(x)
